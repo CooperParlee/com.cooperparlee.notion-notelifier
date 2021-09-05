f4c8baa019164b98433b4a34ae096a43f3efe624
Cooper Parlee <cooper@cooperparlee.com>
2021-09-05 10:04:55 -0400

    Added notionDB to gitignore

--- notelify.py
+++ notelify.py
@@ -2,7 +2,13 @@ from ifttt_webhook import IftttWebhook
 from pathlib import Path
 
 path = "C:/Users/Cooper/OneDrive/Desktop/Projects/notion-notelifier/"
-event_values = {'value1': 'Value for value1',  'value3': 'Value for value3'}
+
+notionDB = ""
+
+with open(path + 'db.txt') as f:
+    notionDB = f.read()
 
 ifttt = IftttWebhook(path + "key.txt")
+
+event_values = {'value1': 'Value for value1',  'value3': 'Value for value3'}
 ifttt.trigger('notion_notelifier', **event_values)
