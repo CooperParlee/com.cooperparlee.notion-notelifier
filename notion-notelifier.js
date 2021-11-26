547096db88987e7fad5e92000240b5c19ffd71be
Cooper Parlee <cooper@cooperparlee.com>
2021-11-25 22:30:27 -0500

    added compileFromDB function

--- notion-notelifier.js
+++ notion-notelifier.js
@@ -1,8 +1,48 @@
 /*
 * Notion Notelifier
 * @author Cooper Parlee
-* 5 September 2021
+* 25 November 2021
 * A simple Javascript application to retrieve Notion book notes, then send them via
-* IFTTT webhooks to my phone.
+* email.
 */
 
+import { Client } from "@notionhq/client"
+import dotenv from 'dotenv'
+import fs from 'fs'
+
+dotenv.config();
+const args = process.argv;
+
+const notion = new Client({auth: process.env.NOTION_KEY});
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+function compileFromDB (dbID, bookName){
+    //Format quote...,pg,book,\n
+    var file = fs.readFile("db.csv");
+    const quoteListDB = await queryDBResponse(dbID);
+    
+    for (const quote of quoteListDB.results){
+        const text;
+        const loc;
+        try {
+            text = quote.properties.Quote.title[0].plain_text;
+            loc = quote.properties.Location.rich_text[0].text.content;
+        } catch (error) { console.error(error); continue; }
+    }
+}
+
+async function queryDBResponse(database) {
+    const response = await notion.databases.query({
+        database_id: database,
+    })
+
+    return response;
+}
+
+async function queryChildPageInfo(id){
+    const response = await notion.blocks.children.list({
+        block_id: id,
+        page_size: 1000,
+    });
+    return response;
+}
